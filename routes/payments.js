3a5c0750e6a5f82e0672ef20248a989b36bbe685
Antoine Cusset <acusset@clever-age.com>
2021-04-02 15:57:46 +0800

    Generate checkout session ID

---
+++ routes/payments.js
@@ -0,0 +1,28 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY)
+const router = require('express').Router();
+
+/** Create paiment session */
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'sgd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://example.com/success',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+  res.json({ id: session.id });
+});
+
+module.exports = router;
